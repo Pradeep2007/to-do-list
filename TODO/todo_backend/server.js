c645fa44ad98f80dbc454eaf47ef24929f00b662
Pradeep Singh Yadav <psy16198@gmail.com>
2025-03-07 00:50:30 +0530

    2

--- TODO/todo_backend/server.js
+++ TODO/todo_backend/server.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-mongoose.connect('mongodb://127.0.0.1:27017/TODO',
+mongoose.connect('mongodb://127.0.0.1:27017/Travel',
     console.log('MongoDB connected')
 )
 
